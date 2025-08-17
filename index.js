33660a6a54d9d9001df00965469788b7c6252059
Natnael3344 <natnael3344@gmail.com>
2025-08-17 21:41:42 +0300

    update1

--- index.js
+++ index.js
@@ -66,3 +66,7 @@ app.post('/submit-form', async (req, res) => {
     });
   }
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
